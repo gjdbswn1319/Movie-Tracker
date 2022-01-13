d00672885b046862e37232339befb125732ca695
gjdbswn1319 <78096395+gjdbswn1319@users.noreply.github.com>
2022-01-13 01:30:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,38 +6,46 @@ tmdb = TMDb()
 tmdb.api_key = 'fcdb87b8e3a4e9fd5a5686f6bcceba17'
 
 movie = Movie()
+discover = Discover()
+person = Person()
+genres = Genre()
 
 movie_log = pd.read_csv('/Users/EllyHur/Desktop/interview/log.csv', sep='|')
 
 app = Flask(__name__)
+
+def getDirector(title):
+	movie_title = movie.search(title)
+	first_result = movie_title[0]
+	credits = movie.credits(first_result.id)
+	for credit in credits.crew:
+		if credit['job'] == 'Director':
+			return credit.name
+
+def getImage(title):
+	movie_title = movie.search(title)
+	first_result = movie_title[0]
+	detail = movie.details(first_result.id)
+	return 'https://image.tmdb.org/t/p/original' + detail.poster_path
 
 @app.route('/')
 @app.route('/home')
 
 
 def home():
-	#return "hello"
-	#return <h1>Hello world</h1>
 	return render_template('index2.html')
 
 @app.route('/result', methods = ['POST', 'GET'])
 def result():
 	director = ''
 	output = request.form.to_dict()
-	print('output: ', output['name'])
-	movie_title = movie.search(output['name'])
-	first_result = movie_title[0]
-	credits = movie.credits(first_result.id)
-	for credit in credits.crew:
-		if credit['job'] == 'Director':
-			director = credit.name
-
-	return render_template('index2.html', name = director)
+	director = getDirector(output['movie_title'])
+	poster = getImage(output['movie_title'])
+	return render_template('index2.html', director = director, poster = poster)
 
 
 if __name__ == "__main__":
 	app.run(debug=True)
-
 
 
 
