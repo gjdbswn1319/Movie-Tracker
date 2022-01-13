c9c87dcd626977594f12451823a0e046301f6d6e
gjdbswn1319 <78096395+gjdbswn1319@users.noreply.github.com>
2022-01-13 02:25:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,18 +15,28 @@ movie_log = pd.read_csv('/Users/EllyHur/Desktop/interview/log.csv', sep='|')
 app = Flask(__name__)
 
 def getDirector(title):
+	director = []
 	movie_title = movie.search(title)
-	first_result = movie_title[0]
-	credits = movie.credits(first_result.id)
-	for credit in credits.crew:
-		if credit['job'] == 'Director':
-			return credit.name
+	#first_result = movie_title[0]
+	for result in movie_title:
+		credits = movie.credits(result.id)
+	#credits = movie.credits(first_result.id)
+		for credit in credits.crew:
+			if credit['job'] == 'Director':
+				director.append(credit.name)
+	return director
 
 def getImage(title):
+	path = []
 	movie_title = movie.search(title)
-	first_result = movie_title[0]
-	detail = movie.details(first_result.id)
-	return 'https://image.tmdb.org/t/p/original' + detail.poster_path
+	for result in movie_title:
+		detail = movie.details(result.id)
+
+		path.append('https://image.tmdb.org/t/p/original' + str(detail.poster_path))
+	#first_result = movie_title[0]
+	#detail = movie.details(first_result.id)
+	#return 'https://image.tmdb.org/t/p/original' + detail.poster_path
+	return path
 
 @app.route('/')
 @app.route('/home')
@@ -36,16 +46,16 @@ def home():
 	return render_template('index2.html')
 
 @app.route('/result', methods = ['POST', 'GET'])
+
 def result():
-	director = ''
+	director = []
+	poster = []
 	output = request.form.to_dict()
 	director = getDirector(output['movie_title'])
 	poster = getImage(output['movie_title'])
+	
 	return render_template('index2.html', director = director, poster = poster)
 
 
 if __name__ == "__main__":
 	app.run(debug=True)
-
-
-
