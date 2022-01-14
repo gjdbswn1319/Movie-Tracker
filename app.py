a1682695506456639f542bcc807fc64bc84ada9d
gjdbswn1319 <78096395+gjdbswn1319@users.noreply.github.com>
2022-01-13 21:54:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, Response, redirect
 from tmdbv3api import *
 import pandas as pd
 
@@ -14,8 +14,17 @@ movie_log = pd.read_csv('/Users/EllyHur/Desktop/interview/log.csv', sep='|')
 
 app = Flask(__name__)
 
+def getID(title):
+	movie_title = movie.search(title)
+
+def getDetail(movie_id, attribute):
+	detail = movie.details(movie_id)
+	result = detail[attribute]
+	return result
+
 def getDirector(title):
 	director = []
+	print(title)
 	movie_title = movie.search(title)
 	#first_result = movie_title[0]
 	for result in movie_title:
@@ -31,14 +40,19 @@ def getImage(title):
 	movie_title = movie.search(title)
 	for result in movie_title:
 		detail = movie.details(result.id)
-
-		path.append('https://image.tmdb.org/t/p/original' + str(detail.poster_path))
+		temp = []
+		temp.append('https://image.tmdb.org/t/p/original' + str(detail.poster_path))
+		temp.append(result.id)
+		path.append(temp)
 	#first_result = movie_title[0]
 	#detail = movie.details(first_result.id)
 	#return 'https://image.tmdb.org/t/p/original' + detail.poster_path
 	return path
 
 @app.route('/')
+
+
+
 @app.route('/home')
 
 
@@ -51,10 +65,22 @@ def result():
 	director = []
 	poster = []
 	output = request.form.to_dict()
+	print(output)
 	director = getDirector(output['movie_title'])
 	poster = getImage(output['movie_title'])
 	
-	return render_template('index2.html', director = director, poster = poster)
+	return render_template('movie_list.html', director = director, poster = poster)
+
+@app.route('/movieInfo', methods = ['POST', 'GET'])
+def movieInfo():
+	output = request.form.to_dict()
+	movieID = output['movie_id']
+	title = getDetail(movieID, 'title')
+	genre = getDetail(movieID, 'genres')
+	overview = getDetail(movieID, 'overview')
+
+	return render_template('movieInfo.html', title=title, genre=genre, overview=overview)
+	
 
 
 if __name__ == "__main__":
